49562d1f8d97177ea32ffa66242304eb2e896aef
eyeyadav31 <eyeyadav31@gmail.com>
2025-05-16 09:21:37 +0530

    Update thumbnails.py

--- EsproMusic/utils/thumbnails.py
+++ EsproMusic/utils/thumbnails.py
@@ -11,7 +11,7 @@ from PIL import ImageFilter, ImageFont, ImageOps
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from NEXIOMUSIC import app
+from EsproMusic import app
 from config import YOUTUBE_IMG_URL
 
 
